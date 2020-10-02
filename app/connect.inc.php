d4afa0750bdff2a7a071d098cebac25a1a4a9ad8
Etienne GILLE <gille@acigne.fr>
2020-10-02 12:13:36 +0200

    GRR est dockerizé et installe automatiquement les tables nécessaires Configuration de php et nginx à revoir suivant tests

---
+++ app/connect.inc.php
@@ -0,0 +1,18 @@
+<?php
+# Les quatre lignes suivantes sont à modifier selon votre configuration
+# ligne suivante : le nom du serveur qui herberge votre base sql.
+# Si c'est le même que celui qui heberge les scripts, mettre "localhost"
+$dbHost="db";
+# ligne suivante : le nom de votre base sql
+$dbDb="{DB_NAME}";
+# ligne suivante : le nom de l'utilisateur sql qui a les droits sur la base
+$dbUser="{DB_USER}";
+# ligne suivante : le mot de passe de l'utilisateur sql ci-dessus
+$dbPass="{DB_PASSWORD}";
+# ligne suivante : préfixe du nom des tables de données
+$table_prefix="grr";
+# ligne suivante : Port MySQL laissé par défaut, valeur numérique
+$dbPort=3306;
+# ligne suivante : adaptation EnvOLE
+$apikey="mypassphrase";
+?>
